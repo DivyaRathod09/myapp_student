e6ce723dcf491d5ec9d0ee51de7f7dbc698674c8
DivyaRathod09 <divyarathod2292002@gmail.com>
2025-09-18 23:06:36 -0700

    change file code

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -13,10 +13,20 @@ class StudentController extends Controller
         return view('students.index', compact('students'));
     }
 
+
+    // public function create()
+    // {
+    //     return view('students.create');
+    // }
+
     public function create()
     {
+        $student = new Student();
+        $student->nonExistingMethod();
+
         return view('students.create');
     }
+
 
     public function store(StudentRequest $request)
     {
@@ -24,19 +34,10 @@ class StudentController extends Controller
         return redirect()->route('students.index')->with('success', 'Student added successfully.');
     }
 
-
     public function destroy(Student $student)
     {
-        try {
-            $student->invalid_column_name;  
-
-            $student->delete();
-
-            return redirect()->route('students.index')->with('success', 'Student deleted successfully.');
-        } catch (\Exception $e) {
-            \Log::error("Delete Student Error: " . $e->getMessage());
-            return redirect()->route('students.index')->with('error', 'Something went wrong while deleting student.');
-        }
+        $student->delete();
+        return redirect()->route('students.index')->with('success', 'Student deleted successfully.');
     }
-
+    
 }
